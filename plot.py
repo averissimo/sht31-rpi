b84c68836d7079726a001dcda96fc5e98ecb0e9b
André Veríssimo <afsverissimo@gmail.com>
2017-11-29 16:07:49 +0000

    improvements on plots

--- plot.py
+++ plot.py
@@ -3,6 +3,9 @@ import datetime
 import dateutil.parser
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+import math
 
 from pymongo import MongoClient
 client = MongoClient()
@@ -22,16 +25,25 @@ for el in results:
 
 time = matplotlib.dates.date2num(time)
 
+sns.set_style('whitegrid', { 'grid.color': '.9'})
+
 fig = plt.figure(figsize=(20,10))
 
 ax1 = fig.add_subplot(121)
 ax2 = fig.add_subplot(122)
 
-ax1.plot_date(time, temp)
-ax2.plot_date(time, humd)
+plt.gcf().autofmt_xdate()
+myFmt = mdates.DateFormatter('%d/%m/%y %H:%M')
 
-ax1.set_ylim(min(temp) - 5, max(temp) + 5)
-ax2.set_ylim(min(humd) - 5, max(humd) + 5)
+ax1.plot_date(time, temp, markersize = 2)
+
+ax2.plot_date(time, humd, markersize = 2)
+
+ax1.xaxis.set_major_formatter(myFmt)
+ax2.xaxis.set_major_formatter(myFmt)
+
+ax1.set_ylim(math.floor(min(temp)) - 2, math.ceil(max(temp)) + 2)
+ax2.set_ylim(math.floor(min(humd)) - 2, math.ceil(max(humd)) + 2)
 
 ax1.set(title="Temperature", xlabel="Time", ylabel="Celsius")
 ax2.set(title="Relative Humidity", xlabel="Time", ylabel="% Relative humidity")
@@ -39,3 +51,5 @@ ax2.set(title="Relative Humidity", xlabel="Time", ylabel="% Relative humidity")
 plt.tight_layout()
 
 plt.savefig("sensor.png")
+
+plt.close()
