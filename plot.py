5fa0aa772e91a8dd4ebd8d946cbeac4ab209634f
André Veríssimo <afsverissimo@gmail.com>
2017-11-29 16:20:22 +0000

    failsafe against corrupt entries

--- plot.py
+++ plot.py
@@ -16,37 +16,44 @@ results = db.readings.find()
 
 temp = []
 humd = []
-time = []
+timeT = []
+timeH = []
 
 for el in results:
+  if 'createdAt' not in el:
+    continue
+  if 'temperature' in el:
     temp.append(el['temperature'])
+    timeT.append(dateutil.parser.parse(el['createdAt']))
+  if 'humidity' in el:
     humd.append(el['humidity'])
-    time.append(dateutil.parser.parse(el['createdAt']))
+    timeH.append(dateutil.parser.parse(el['createdAt']))
 
-time = matplotlib.dates.date2num(time)
+
+timeT = matplotlib.dates.date2num(timeT)
+timeH = matplotlib.dates.date2num(timeH)
 
 sns.set_style('whitegrid', { 'grid.color': '.9'})
 
 fig = plt.figure(figsize=(20,10))
 
-ax1 = fig.add_subplot(121)
-ax2 = fig.add_subplot(122)
-
-plt.gcf().autofmt_xdate()
 myFmt = mdates.DateFormatter('%d/%m/%y %H:%M')
 
-ax1.plot_date(time, temp, markersize = 2)
+if len(temp) > 0:
+  ax1 = fig.add_subplot(121)
+  plt.gcf().autofmt_xdate()
+  ax1.plot_date(timeT, temp, markersize = 2)
+  ax1.xaxis.set_major_formatter(myFmt)
+  ax1.set_ylim(math.floor(min(temp)) - 2, math.ceil(max(temp)) + 2)
+  ax1.set(title="Temperature", xlabel="Time", ylabel="Celsius")
 
-ax2.plot_date(time, humd, markersize = 2)
-
-ax1.xaxis.set_major_formatter(myFmt)
-ax2.xaxis.set_major_formatter(myFmt)
-
-ax1.set_ylim(math.floor(min(temp)) - 2, math.ceil(max(temp)) + 2)
-ax2.set_ylim(math.floor(min(humd)) - 2, math.ceil(max(humd)) + 2)
-
-ax1.set(title="Temperature", xlabel="Time", ylabel="Celsius")
-ax2.set(title="Relative Humidity", xlabel="Time", ylabel="% Relative humidity")
+if len(humd) > 0:
+  ax2 = fig.add_subplot(122)
+  plt.gcf().autofmt_xdate()
+  ax2.plot_date(timeH, humd, markersize = 2)
+  ax2.xaxis.set_major_formatter(myFmt)
+  ax2.set_ylim(math.floor(min(humd)) - 2, math.ceil(max(humd)) + 2)
+  ax2.set(title="Relative Humidity", xlabel="Time", ylabel="% Relative humidity")
 
 plt.tight_layout()
 
