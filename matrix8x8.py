3508145c2e23ccb0f2992c83a4d045cdfeb3d5bd
André Veríssimo <afsverissimo@gmail.com>
2018-03-28 02:22:13 +0100

    major update

---
+++ matrix8x8.py
@@ -0,0 +1,145 @@
+import time
+
+from PIL import Image
+from PIL import ImageDraw
+
+from Adafruit_LED_Backpack import Matrix8x8
+
+# Draw some shapes using the Python Imaging Library.
+
+
+
+def draw(val, type = ''):
+    def digit(d, x0 = 0, y0 = 0):
+        options = {0: zero, 1: one, 2: two, 3: three,
+                   4: four, 5: five, 6: six, 7: seven,
+                   8: eight, 9: nine}
+        options[d](x0, y0)
+
+    def zero(x0,y0):
+        draw.line((0+x0,0+y0,0+x0,4+y0), fill=255)
+        draw.line((1+x0,0+y0,1+x0,4+y0), fill=255)
+
+    def one(x0, y0):
+        ## One
+        draw.line((1+x0,0+y0,1+x0,4+y0), fill=255)
+
+    def two(x0, y0):
+        ## Two
+        draw.line((0+x0,0+y0,1+x0,0+y0), fill=255)
+        draw.line((1+x0,0+y0,1+x0,2+y0), fill=255)
+        draw.line((1+x0,2+y0,0+x0,2+y0), fill=255)
+        draw.line((0+x0,2+y0,0+x0,4+y0), fill=255)
+        draw.line((0+x0,4+y0,1+x0,4+y0), fill=255)
+
+    def three(x0, y0):
+        ## Three
+        draw.line((0+x0,0+y0,1+x0,0+y0), fill=255)
+        draw.line((1+x0,0+y0,1+x0,2+y0), fill=255)
+        draw.line((1+x0,2+y0,0+x0,2+y0), fill=255)
+        draw.line((1+x0,2+y0,1+x0,4+y0), fill=255)
+        draw.line((1+x0,4+y0,0+x0,4+y0), fill=255)
+
+    def four(x0, y0):
+        draw.line((0+x0,0+y0,0+x0,2+y0), fill=255)
+        draw.line((1+x0,2+y0,1+x0,4+y0), fill=255)
+
+    def five(x0, y0):
+        draw.line((0+x0,0+y0,1+x0,0+y0), fill=255)
+        draw.line((0+x0,0+y0,0+x0,2+y0), fill=255)
+        draw.line((1+x0,2+y0,0+x0,2+y0), fill=255)
+        draw.line((1+x0,2+y0,1+x0,4+y0), fill=255)
+        draw.line((0+x0,4+y0,1+x0,4+y0), fill=255)
+
+    def six(x0, y0):
+        draw.line((0+x0,0+y0,0+x0,4+y0), fill=255)
+        draw.line((1+x0,2+y0,1+x0,4+y0), fill=255)
+
+    def seven(x0, y0):
+        draw.line((0+x0,0+y0,1+x0,0+y0), fill=255)
+        draw.line((1+x0,1+y0,1+x0,4+y0), fill=255)
+
+    def eight(x0, y0):
+        draw.line((0+x0,0+y0,1+x0,0+y0), fill=255)
+        draw.line((0+x0,1+y0,1+x0,1+y0), fill=255)
+        draw.line((0+x0,3+y0,1+x0,3+y0), fill=255)
+        draw.line((0+x0,4+y0,1+x0,4+y0), fill=255)
+
+    def nine(x0, y0):
+        draw.line((1+x0,0+y0,1+x0,4+y0), fill=255)
+        draw.line((0+x0,0+y0,0+x0,2+y0), fill=255)
+
+    def negative(x0 = 0, y0 = 0):
+        draw.line((0+x0,2+y0,1+x0,2+y0), fill=255)
+
+    def error_letter(x0 = 0, y0 = 0):
+        draw.line((0+x0,0+y0,2+x0,0+y0), fill=255)
+        draw.line((0+x0,2+y0,1+x0,2+y0), fill=255)
+        draw.line((0+x0,4+y0,1+x0,4+y0), fill=255)
+        draw.line((0+x0,0+y0,0+x0,4+y0), fill=255)
+        #
+        draw.line((3+x0,2+y0,3+x0,4+y0), fill=255)
+        draw.line((4+x0,2+y0,4+x0,2+y0), fill=255)
+        #
+        draw.line((6+x0,2+y0,6+x0,4+y0), fill=255)
+        draw.line((7+x0,2+y0,7+x0,2+y0), fill=255)
+
+    def h_letter(x0 = 0, y0 = 0):
+        draw.line((0+x0,0+y0,0+x0,2+y0), fill=255)
+        draw.line((0+x0,1+y0,2+x0,1+y0), fill=255)
+        draw.line((2+x0,0+y0,2+x0,2+y0), fill=255)
+
+    def t_letter(x0 = 0, y0 = 0):
+        draw.line((0+x0,0+y0,2+x0,0+y0), fill=255)
+        draw.line((1+x0,0+y0,1+x0,2+y0), fill=255)
+
+    def getNum(val):
+        isneg = 0
+        if val < 0:
+            isneg = 1
+            val = abs(val)
+
+        valstr = str(int(round(val)))
+
+        if len(valstr) == 1:
+            valstr = '0' + valstr
+
+        if len(valstr) == 3 and isneg == 1:
+            error_letter(0, 3)
+        elif(len(valstr) == 3):
+            digit(int(valstr[0]), 0, 3)
+            digit(int(valstr[1]), 3, 3)
+            digit(int(valstr[2]), 6, 3)
+        elif(isneg == 1):
+            negative(0,3)
+            digit(int(valstr[0]), 3, 3)
+            digit(int(valstr[1]), 6, 3)
+        else:
+            digit(int(valstr[0]), 3, 3)
+            digit(int(valstr[1]), 6, 3)
+
+    # Create display instance on default I2C address (0x70) and bus number.
+    display = Matrix8x8.Matrix8x8()
+
+    # Initialize the display. Must be called once before using the display.
+    display.begin()
+
+    # Clear the display buffer.
+    display.clear()
+
+    # First create an 8x8 1 bit color image.
+    image = Image.new('1', (8, 8))
+
+    # Then create a draw instance.
+    draw = ImageDraw.Draw(image)
+    if type == 'temp':
+        t_letter()
+    elif type == 'humd':
+        h_letter()
+    getNum(val)
+
+    # Draw the image on the display buffer.
+    display.set_image(image)
+
+    # Draw the buffer to the display hardware.
+    display.write_display()
--- sensor.py
+++ sensor.py
@@ -3,39 +3,41 @@ import time
 import datetime
 import pymongo
 
-# Get I2C bus
-bus = smbus.SMBus(1)
+def retrieve():
+  # Get I2C bus
+  bus = smbus.SMBus(1)
  
-# SHT31 address, 0x44(68)
-bus.write_i2c_block_data(0x45, 0x2C, [0x06])
+  # SHT31 address, 0x44(68)
+  bus.write_i2c_block_data(0x45, 0x2C, [0x06])
  
-time.sleep(0.5)
+  time.sleep(0.5)
  
-# SHT31 address, 0x44(68)
-# Read data back from 0x00(00), 6 bytes
-# Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
-data = bus.read_i2c_block_data(0x45, 0x00, 6)
+  # SHT31 address, 0x44(68)
+  # Read data back from 0x00(00), 6 bytes
+  # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+  data = bus.read_i2c_block_data(0x45, 0x00, 6)
  
-# Convert the data
-temp = data[0] * 256 + data[1]
-cTemp = -45 + (175 * temp / 65535.0)
-humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+  # Convert the data
+  temp = data[0] * 256 + data[1]
+  cTemp = -45 + (175 * temp / 65535.0)
+  humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+  
+  
+  return {
+    "temperature": cTemp,
+    "humidity": humidity,
+    "createdAt": datetime.datetime.utcnow()
+  }
+   
 
-from pymongo import MongoClient
-client = MongoClient()
+def savemongo():
+  
+  reading = retrieve()
 
-db = client.sensor
+  from pymongo import MongoClient
+  client = MongoClient()
 
-reading = {
-        "temperature": cTemp,
-        "humidity": humidity,
-        "createdAt": datetime.datetime.utcnow()
-        }
+  db = client.sensor
 
-db.readings.insert_one(reading)
-
-# Output data to screen
-print "Temperature in Celsius is : %.2f C" %cTemp
-print "Relative Humidity is : %.2f %%RH" %humidity
-#print "  at %s" %newReading.createdAt
+  db.readings.insert_one(reading)
 
